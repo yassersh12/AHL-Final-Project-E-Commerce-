c33c0e702615d5c970b0718fe4e4f6a50b467f1d
msamaheen-ahl <sami2011643@gmail.com>
2024-09-25 20:33:41 +0300

    add login prefix

--- app/api/main.py
+++ app/api/main.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.api.routes import user, login, status, order
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
+api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(status.router, prefix="/statuses", tags=["statuses"])
 api_router.include_router(order.router, prefix="/orders", tags=["orders"])
