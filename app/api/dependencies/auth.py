003ae1d587d463b31aa0b21d19ab330ad03e0613
yasser sharabati <yassersharabati2003@gmail.com>
2024-10-19 18:23:14 +0300

    edit the user-endpoints security and add get current active admin method

--- app/api/dependencies/auth.py
+++ app/api/dependencies/auth.py
@@ -95,3 +95,21 @@ async def get_current_active_user(
     except Exception as e:
         print(f"An error occurred while getting current active user: {e}")
         raise HTTPException(status_code=400, detail="Error getting current active user")
+
+
+async def get_current_active_admin(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    try:
+        if not current_user.active:
+            raise HTTPException(status_code=400, detail="Inactive user")
+        if not current_user.is_admin:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Not enough privileges"
+            )
+        return current_user
+    except Exception as e:
+        print(f"An error occurred while getting current active admin: {e}")
+        raise HTTPException(
+            status_code=400, detail="Error getting current active admin"
+        )
--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -10,18 +10,36 @@ from app.api.exceptions.global_exceptions import (
     InvalidUUIDException,
 )
 from uuid import UUID
+from app.api.dependencies.auth import *
 
 router = APIRouter()
 
 
-@router.post("/", response_model=UserResponse)
-def create_user(user: UserCreateRequest, db: Session = Depends(get_db)):
+# this method for normal user
+@router.post("/register", response_model=UserResponse)
+def register_user(user: UserCreateRequest, db: Session = Depends(get_db)):
+    service = UserService(db)
+    user_dict = user.model_dump()
+    user_dict["is_admin"] = False
+    return service.create_user(UserCreateRequest(**user_dict))
+
+
+# this method only for admins to regiter users
+def create_user(
+    user: UserCreateRequest,
+    db: Session = Depends(get_db),
+    current_admin: User = Depends(get_current_active_admin),
+):
     service = UserService(db)
     return service.create_user(user)
 
 
 @router.get("/{user_id}", response_model=UserResponse)
-def get_user(user_id: str, db: Session = Depends(get_db)):
+def get_user(
+    user_id: str,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
     try:
         user_uuid = UUID(user_id)
     except ValueError:
@@ -38,7 +56,10 @@ def get_user(user_id: str, db: Session = Depends(get_db)):
 
 @router.put("/{user_id}", response_model=UserResponse)
 def update_user(
-    user_id: str, user_data: UserUpdateRequest, db: Session = Depends(get_db)
+    user_id: str,
+    user_data: UserUpdateRequest,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
 ):
     try:
         user_uuid = UUID(user_id)
@@ -50,7 +71,11 @@ def update_user(
 
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: str, db: Session = Depends(get_db)):
+def delete_user(
+    user_id: str,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
     try:
         user_uuid = UUID(user_id)
     except ValueError:
@@ -66,9 +91,9 @@ def delete_user(user_id: str, db: Session = Depends(get_db)):
 
 
 @router.get("/", response_model=list[UserResponse])
-def get_all_users(db: Session = Depends(get_db)):
+def get_all_users(
+    db: Session = Depends(get_db),
+    current_admin: User = Depends(get_current_active_admin),
+):
     service = UserService(db)
     return service.get_all_users()
-
-
-#
