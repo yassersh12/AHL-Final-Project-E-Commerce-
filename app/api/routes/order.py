e8438c519e759d40c56eeaf3585f4f0c9419ae43
Hatem29 <hatm.h.s2011@hotmail.com>
2024-10-20 19:39:01 +0300

    Resolved pull request comments in exception handing and status codes.

--- app/api/routes/order.py
+++ app/api/routes/order.py
@@ -15,11 +15,7 @@ order_service = OrderService()
 def create_order(orderItems : List[OrderItem]):
     try:
         order = order_service.create_order(order_items= orderItems)
-    except ProductDoesNotExistException as notExits:
-        raise notExits
-    except OutOfStockException as noStock:
-        raise noStock
-    except Exception as e:
+    except Exception:
         raise InternalServerErrorException()
 
     return JSONResponse(
@@ -31,9 +27,7 @@ def create_order(orderItems : List[OrderItem]):
 def get_order_details(order_id: UUID):
     try:
         order_details = order_service.get_order_by_id(order_id)
-    except HTTPException as e:
-        raise e
-    except Exception as e:
+    except Exception:
         raise InternalServerErrorException()
     
     return order_details
@@ -43,9 +37,8 @@ def update_order_status(order_id: UUID, status_name: str):
     # TODO: Implement admin authorization check when dependency is available
     try:
         updated_order_response = order_service.update_order_status(order_id, status_name)
-    except HTTPException as e:
-        raise e
-    except Exception as e:
+
+    except Exception:
         raise InternalServerErrorException()
     
     #Returns 200 OK
@@ -54,15 +47,10 @@ def update_order_status(order_id: UUID, status_name: str):
 @router.delete("/orders/{order_id}", status_code=status.HTTP_204_NO_CONTENT)
 def cancel_order(order_id: UUID):
     # TODO: Implement user authentication check when dependency is available
-
     try:
         order_service.cancel_order(order_id)
-    except HTTPException as e:
-        raise e
     except Exception as e:
         raise InternalServerErrorException()
 
-    # Returns 204 No Content
-    return None
 
 
